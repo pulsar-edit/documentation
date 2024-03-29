4ad43507b998bec0846f8149eafd07ca09cdb467
Andrew Dupont <github@andrewdupont.net>
2024-03-28 23:34:05 -0700

    Compare URLs for next/previous, not article titles

--- helpers/index.js
+++ helpers/index.js
@@ -2,10 +2,11 @@
 // View helpers. Available in EJS templates via the `helpers` namespace.
 
 module.exports = {
-  nextAndPreviousSidebarEntries(title, sidebar) {
+  nextAndPreviousSidebarEntries(url, sidebar) {
     let prev = null, next = null;
+    if (!url) return [prev, next];
     let lastIndex = sidebar.length - 1;
-    let index = sidebar.findIndex(entry => entry.text === title);
+    let index = sidebar.findIndex(entry => entry.link === url || `${entry.link}/` === url);
     if (index) {
       if (index > 0) prev = sidebar[index - 1];
       if (index < lastIndex) next = sidebar[index + 1];
