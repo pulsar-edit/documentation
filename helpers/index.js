29dd387a02e5d4961675dbd1b1f72b80cb9a2d16
confused-Techie <dev@lhbasics.com>
2024-07-22 22:16:06 -0700

    Display all pulsar api versions as list

--- helpers/index.js
+++ helpers/index.js
@@ -1,5 +1,7 @@
-
 // View helpers. Available in EJS templates via the `helpers` namespace.
+
+const fs = require("fs");
+const semver = require("semver");
 
 module.exports = {
   nextAndPreviousSidebarEntries(url, sidebar) {
@@ -12,5 +14,33 @@ module.exports = {
       if (index < lastIndex) next = sidebar[index + 1];
     }
     return [prev, next];
+  },
+
+  getAllPulsarVersions() {
+    // Returns an array of Pulsar semver versions, in order, and without 'latest'
+
+    let verArr = [];
+
+    const verDocs = fs.readdirSync("./pulsar-api/content");
+
+    for (const doc of verDocs) {
+      let clean = doc.replace(".json", "");
+
+      let name = clean;
+
+      if (!name.startsWith("v")) {
+        name = "v" + name;
+      }
+
+      verArr.push({
+        name: name,
+        link: clean
+      });
+    }
+
+    // Sort version array in order
+    verArr.sort((a, b) => { return semver.rcompare(a.name, b.name); });
+
+    return verArr;
   }
 };
